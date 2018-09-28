e0b2e9712e7c164b444122a2e70dbbb40dce53eb
Q-Mart <qumarthjash@gmail.com>
2018-09-28 17:01:53 +0100

    Fill gaps for test

--- addition-gaps/cmd/test-addition/main.go
+++ addition-gaps/cmd/test-addition/main.go
@@ -25,11 +25,11 @@ func main() {
 	// Pass the arguments to the kernel
 
 	// Set the first operand to 1
-	// YOUR CODE HERE
+	krnl.SetArg(0, 1)
 	// Set the second operand to 2
-	// YOUR CODE HERE
+	krnl.SetArg(1, 2)
 	// Set the pointer to the output buffer
-	// YOUR CODE HERE
+	krnl.SetMemoryArg(2, buff)
 
 	// Run the FPGA with the supplied arguments. This is the same for all projects.
 	// The arguments ``(1, 1, 1)`` relate to x, y, z co-ordinates and correspond to our current
@@ -45,10 +45,12 @@ func main() {
 	}
 
 	// Print the value we got from the FPGA
-	// YOUR CODE HERE
+	fmt.Printf("1 + 2 = %d according to the FPGA", ret)
 
 	// Check the result is correct and if not, return an error
 	// YOUR CODE HERE: create an if statment to exit if the value retuned does not equal 3
-	//
-	//
+	if ret != 3 {
+		fmt.Printf("%d != 3", ret)
+		os.Exit(1)
+	}
 }
